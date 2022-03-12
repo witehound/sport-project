28b5b3b0e70e6f2c0a22fa94c35553798a10edc8
InventedMost <netvid00@gmail.com>
2022-03-12 13:18:48 +0100

    making product page functional

---
+++ cart.js
@@ -0,0 +1,5 @@
+import { addedToCart } from "./product";
+
+const fortest = document.getElementsByClassName('fortest')
+fortest.addEventListener('click',
+()=> console.log(addedToCart))
--- product.js
+++ product.js
@@ -1,5 +1,4 @@
 // selecting elements that we need
-// const productsContainer = document.getElementsByClassName('products')
 const renderContainer = document.querySelector('.products');
 const adidas = document.querySelector('.adidas');
 const newBalance = document.querySelector('.newBalance');
@@ -7,6 +6,8 @@ const puma = document.querySelector('.puma');
 const nike = document.querySelector('.nike');
 const underArmour = document.querySelector('.underArmour');
 const allbrands = document.querySelector('.allbrands');
+const cart = document.querySelector('.cart');
+const compareStatic = document.querySelector('.compare_static')
 
 
 
@@ -17,38 +18,35 @@ const product_array = [
       'price': 200,
       'color': 'white',
       'brand': 'nike',
-      'image': 'https://www.thenextsole.com/storage/images/CV8480-300.png'
+      'image': 'https://www.thenextsole.com/storage/images/CV8480-300.png',
+      'state':''
     },
     {
       'name': 'crocs',
       'price': 850,
       'color': 'brown',
       'brand': 'adidas',
-      'image': 'https://assets.adidas.com/images/w_600,f_auto,q_auto/f544367209ae4494b5f0ab730140c5af_9366/Adilette_Clogs_White_FY8970_01_standard.jpg'
+      'image': 'https://assets.adidas.com/images/w_600,f_auto,q_auto/f544367209ae4494b5f0ab730140c5af_9366/Adilette_Clogs_White_FY8970_01_standard.jpg',
+      'state':''
     },
     {
       'name': 'sneaker',
       'price': 549,
       'color': 'red',
       'brand': 'puma',
-      'image': 'https://www.ikarinotekken.com/images/h/red%20puma%20sneakers-210lrh.jpg'
+      'image': 'https://www.ikarinotekken.com/images/h/red%20puma%20sneakers-210lrh.jpg',
+      'state':''
     }
   ];
 
   const addedToCart = []
 
+  const forToCompare = []
 
-  //for addtocart
-  let finalItemId 
-//   const letsAdd = (item) => {
-//     finalItemId =item
-//     return finalItemId
-//   }  
 
-//   console.log(finalItemId)
 
   //function to render element that holds each from from product_array
-  const renderAnItem = (item,container,finalItemId) => {
+  const renderAnItem = (item,container) => {
      
       const product_div = document.createElement('div')
       product_div.setAttribute('class', 'product_div')
@@ -64,7 +62,6 @@ const product_array = [
       const productBottom = document.createElement('div')
       productBottom.setAttribute('class', 'product_div_buttom')
      
-
       const productBottomH2 = document.createElement('h2')
       productBottomH2.setAttribute('class', 'product_div_buttom_h2')
       productBottomH2.innerText = item.name;
@@ -77,12 +74,37 @@ const product_array = [
       productPrice.setAttribute('class','product_div_price')
       productPrice.innerText = item.price;
 
-    
+      const toCompare = document.createElement('button')
+      toCompare.setAttribute('class', 'to_compare')
+      toCompare.innerText = 'Compare'
+      toCompare.addEventListener('click',()=>{
+        if(forToCompare.length < 2){
+          forToCompare.push(item)
+          console.log(push)
+          if(forToCompare.length > 1){
+            if(forToCompare[0].name === forToCompare[1].name){
+              forToCompare.splice(forToCompare[0])
+              console.log(forToCompare[1])
+            }
+          }
+      }else{
+        forToCompare.shift()
+        forToCompare.push(item)
+      }}
+      )
+
       const addTocart = document.createElement('button')
       addTocart.setAttribute('class','product_div_add')
       addTocart.innerText = '+'
+      addTocart.addEventListener('click',()=>{
+        addedToCart.push(item)
+      })
 
-    //   letsAdd(item)
+      if(item.state === ""){
+        addTocart.style.display = 'block'
+      }else{
+        addTocart.style.display = 'none'
+      }
 
       container.appendChild(product_div);
       product_div.appendChild(productTop);
@@ -91,25 +113,16 @@ const product_array = [
       productBottom.appendChild(productBottomH2)
       productBottom.appendChild(productBottomP)
       productBottom.appendChild(productPrice)
+      productPrice.appendChild(toCompare)
       productPrice.appendChild(addTocart)
-
-      finalItemId = item
-
   }
 
-  //eventlistener for add to cart button
-//   const addTocart = document.getElementsByClassName('product_div_add')
-//   addTocart.addEventListener('click', ()=>{
-//       console.log('button clicked')
-//   })
-console.log(finalItemId)
 
 
-// using the product_array to render our products
+// using the product_array to render our products on start
   product_array.forEach(product => {
     renderAnItem(product, renderContainer);
   });
-
 
   // function to filter our brands
   const pickSpecificBrand = (item, brandName) => {
@@ -117,53 +130,36 @@ console.log(finalItemId)
       return specificBrand
   }
 
-  //adding eventlisters to filter brands
-  allbrands.addEventListener('click',()=>{
+  //functions to addeventlisteners to buttons
+  const initialFetch = (title,array,state,) => {
+  title.addEventListener('click',()=>{
     renderContainer.innerHTML = '';
-    product_array.forEach(product => {
+    array.forEach(product => {
+        product.state = state
         renderAnItem(product, renderContainer);
       });
-  })
+  })}
 
-  adidas.addEventListener('click',()=>{
+  const fetcher = (title,array,titletype,state) => {
+  title.addEventListener('click',()=>{
     renderContainer.innerHTML = '';
-    const adidasProducts = pickSpecificBrand(product_array,'adidas')
+    const adidasProducts = pickSpecificBrand(array,titletype)
     adidasProducts.forEach(product => {
+        product.state = state
         renderAnItem(product, renderContainer);
       });
-  })
-  
-  newBalance.addEventListener('click',()=>{
-    renderContainer.innerHTML = '';
-    const adidasProducts = pickSpecificBrand(product_array,'newbalance')
-    adidasProducts.forEach(product => {
-        renderAnItem(product, renderContainer);
-      });
-  })
+  })}
 
-  puma.addEventListener('click',()=>{
-    renderContainer.innerHTML = '';
-    const adidasProducts = pickSpecificBrand(product_array,'puma')
-    adidasProducts.forEach(product => {
-        renderAnItem(product, renderContainer);
-      });
-  })
-
-  nike.addEventListener('click',()=>{
-    renderContainer.innerHTML = '';
-    const adidasProducts = pickSpecificBrand(product_array,'nike')
-    adidasProducts.forEach(product => {
-        renderAnItem(product, renderContainer);
-      });
-  })
+  // calling functions for our buttons
+  initialFetch(allbrands,product_array,'')
+  initialFetch(cart,addedToCart,'cart')
+  fetcher(adidas,product_array,'adidas','')
+  fetcher(nike,product_array,'nike','')
+  fetcher(puma,product_array,'puma','')
+  fetcher(underArmour,product_array,'underarmour','')
+  fetcher(newBalance,product_array,'newbalance','')
   
-  underArmour.addEventListener('click',()=>{
-    renderContainer.innerHTML = '';
-    const adidasProducts = pickSpecificBrand(product_array,'underarmour')
-    adidasProducts.forEach(product => {
-        renderAnItem(product, renderContainer);
-      });
-  })
-  
-  console.log(addedTocart)
-  
+// to compare
+compareStatic.addEventListener('click',()=> {
+  console.log(forToCompare)
+})
