810b27b225cd94484fb2dfe285b83632fd24f4cb
InventedMost <netvid00@gmail.com>
2022-04-13 16:55:20 +0100

    Update product.js

--- src/javascript/product.js
+++ src/javascript/product.js
@@ -350,7 +350,7 @@ const renderAnItem = (item, container) => {
   productPrice.appendChild(toCompare);
   productPrice.appendChild(addTocart);
 };
-//ready
+//ready player
 
 const renderToCompare = (item, container) => {
   const letsCompare = document.createElement("div");
